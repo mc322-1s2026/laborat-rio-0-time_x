ddfc0e96a568441e5227888f80365b0d223afd8f
Reis, D <83190659+GloomyWraith@users.noreply.github.com>
2026-03-04 01:44:45 +0000

    Usuario u1 adicionado e setado como chefe. U2 se torna o executor da tarefa.

--- src/main/java/com/laboratorio0/Main.java
+++ src/main/java/com/laboratorio0/Main.java
@@ -2,7 +2,7 @@ package com.laboratorio0;
 
 public class Main {
     public static void main(String[] args) {
-        Usuario u1 = new Usuario("", "ana@email.com");
+        Usuario u1 = new Usuario("Daniel Reis", "d169250@dac.unicamp.br");
         Usuario u2 = new Usuario("Carlos", "carlos@email.com"); // Perfil do chefe
 
         Chefe chefe = new Chefe(u2);
